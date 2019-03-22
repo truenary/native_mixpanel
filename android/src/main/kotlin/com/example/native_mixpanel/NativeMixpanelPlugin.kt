1622216023b6a04b3dfdaae124b1ac92b81d2a15
Sovit <sovit.poudel@gmail.com>
2019-03-22 13:15:29 +0545

    NativeMixpanelPlugin.kt: implement methods for initializing and tracking events

--- android/src/main/kotlin/com/example/native_mixpanel/NativeMixpanelPlugin.kt
+++ android/src/main/kotlin/com/example/native_mixpanel/NativeMixpanelPlugin.kt
@@ -1,5 +1,8 @@
 package com.example.native_mixpanel
 
+import android.content.Context
+import android.util.Log
+import com.mixpanel.android.mpmetrics.MixpanelAPI
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -7,9 +10,15 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class NativeMixpanelPlugin: MethodCallHandler {
+
+  private var mixpanel: MixpanelAPI? = null
+
   companion object {
+
+    var ctxt: Context? = null
     @JvmStatic
     fun registerWith(registrar: Registrar) {
+      ctxt = registrar.context()
       val channel = MethodChannel(registrar.messenger(), "native_mixpanel")
       channel.setMethodCallHandler(NativeMixpanelPlugin())
     }
@@ -18,8 +27,14 @@ class NativeMixpanelPlugin: MethodCallHandler {
   override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    }
+    else if (call.method == "initialize") {
+      mixpanel = MixpanelAPI.getInstance(ctxt, call.arguments.toString())
+      result.success("Init success..")
+    }
+    else {
+      mixpanel?.track(call.method)
+      result.success("Track success..")
     }
   }
 }
