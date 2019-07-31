168957ab6ce0902dd4cf25b6d00a3b06a81325d2
Suren <surendra.lama@assetowl.com>
2019-07-31 17:25:41 +0545

    ios: move reading properties from arg of method call to separate function

--- ios/Classes/SwiftNativeMixpanelPlugin.swift
+++ ios/Classes/SwiftNativeMixpanelPlugin.swift
@@ -11,6 +11,21 @@ import Mixpanel
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  public func getPropertiesFromArguments(callArguments: Any?) throws -> Properties? {
+
+    if let arguments = callArguments, let data = (arguments as! String).data(using: .utf8) {
+
+      let properties = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+      var argProperties = [String: String]()
+      for (key, value) in properties {
+        argProperties[key] = String(describing: value)
+      }
+      return argProperties;
+    }
+
+    return nil;
+  }
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult)  {
     do {
       
@@ -20,14 +35,8 @@ import Mixpanel
         Mixpanel.mainInstance().identify(distinctId: call.arguments as! String)
       } else if(call.method == "alias") {
         Mixpanel.mainInstance().createAlias(call.arguments as! String, distinctId: Mixpanel.mainInstance().distinctId)
-      } else if let arguments = call.arguments, let data = (arguments as! String).data(using: .utf8) {
-
-        let properties = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-        var sendProperties = [String: String]()
-        for (key, value) in properties {
-            sendProperties[key] = String(describing: value)
-        }
-        Mixpanel.mainInstance().track(event: call.method, properties: sendProperties)
+      } else if let argProperties = try self.getPropertiesFromArguments(callArguments: call.arguments) {
+        Mixpanel.mainInstance().track(event: call.method, properties: argProperties)
       } else {
         Mixpanel.mainInstance().track(event: call.method)
       }
